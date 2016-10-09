aa11b73dd5fed057b6c01cfa06b762964af8c9ed
Artur Olszak <aolszak@gmail.com>
2016-10-09 21:40:12 +0200

    added creating and deletig tmp folder for documentation generation

--- src/documentation/protocol_doc_tex/pdf.bat
+++ src/documentation/protocol_doc_tex/pdf.bat
@@ -1,7 +1,9 @@
 @echo off
+mkdir tmp
 pdflatex -file-line-error-style -output-directory=tmp -aux-directory=tmp -include-directory=tex tex/main.tex
 pdflatex -file-line-error-style -output-directory=tmp -aux-directory=tmp -include-directory=tex tex/main.tex
 bibtex -min-crossrefs -1 tmp/main
 pdflatex -file-line-error-style -output-directory=tmp -aux-directory=tmp -include-directory=tex tex/main.tex
 pdflatex -file-line-error-style -output-directory=tmp -aux-directory=tmp -include-directory=tex tex/main.tex
-move tmp\main.pdf HyCube_protocol_documentation.pdf
+move tmp\main.pdf ..\..\..\HyCube_protocol_documentation.pdf
+rmdir /Q /S tmp
